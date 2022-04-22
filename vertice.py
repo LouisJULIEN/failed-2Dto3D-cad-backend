3afa4a0b428fe7c10719cd510bff8a55117ab267
louisjulien <louis69600@laposte.net>
2022-04-22 11:08:23 +0200

    remove useless duplicates discarder

--- vertice.py
+++ vertice.py
@@ -38,7 +38,6 @@ def find_candidate_vertices(parsed_two_D_projections: parsed_2D):
                         an_xz_point.link_to(first_point)
                         an_yz_point.link_to(first_point)
 
-    found_3D_points = remove_duplicated_reconstructed_points(found_3D_points)
     dandling_points = []
 
     for a_project_point in edges_xy + edges_xz + edges_yz:
@@ -46,28 +45,3 @@ def find_candidate_vertices(parsed_two_D_projections: parsed_2D):
             dandling_points.append(a_project_point)
 
     return found_3D_points, dandling_points
-
-
-def remove_duplicated_reconstructed_points(found_3D_points):
-    # Remove duplicates created by aligned in an ignored direction
-    # for example two an_xy_point with different z value
-
-    def get_coordinates(item1):
-        return item1.x, item1.y, item1.z
-
-    found_3D_points.sort(key=get_coordinates)
-
-    i = 0
-    while i < len(found_3D_points) - 1:
-        if found_3D_points[i].distance(found_3D_points[i + 1]) < MAX_POINT_TO_POINT_ERROR_DISTANCE:
-
-            point_with_links_to_remove = found_3D_points[i + 1].links - found_3D_points[i].links
-            for a_point_with_link_to_remove in point_with_links_to_remove:
-                a_point_with_link_to_remove.remove_link(found_3D_points[i + 1])
-
-            found_3D_points[i].link_to_multiples(found_3D_points[i + 1].links)
-            print(f"{list(found_3D_points[i].coords)} {list(found_3D_points[i + 1].coords)}")
-            del found_3D_points[i + 1]
-        else:
-            i += 1
-    return found_3D_points
